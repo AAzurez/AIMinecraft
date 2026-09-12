4cf329829c6dd8a3d4c049ff0be3df29d269ab85
AAzurez <aaronli5370@gmail.com>
2026-09-12 02:05:39 -0400

    Testing Bot

--- examples/python/basic.py
+++ examples/python/basic.py
@@ -3,7 +3,7 @@ mineflayer = require('mineflayer')
 pathfinder = require('mineflayer-pathfinder')
 
 RANGE_GOAL = 1
-BOT_USERNAME = 'python'
+BOT_USERNAME = 'Jerry'
 
 bot = mineflayer.createBot({
   'host': '127.0.0.1',
@@ -20,10 +20,10 @@ def handle(*args):
   movements = pathfinder.Movements(bot)
 
   @On(bot, 'chat')
-  def handleMsg(this, sender, message, *args):
+  def handleMsg( sender, message, *args):
     print("Got message", sender, message)
-    if sender and (sender != BOT_USERNAME):
-      bot.chat('Hi, you said ' + message)
+    if sender != BOT_USERNAME:
+      bot.chat(f'Hi, you said "{message}"')
       if 'come' in message:
         player = bot.players[sender]
         print("Target", player)
@@ -31,7 +31,6 @@ def handle(*args):
         if not target:
           bot.chat("I don't see you !")
           return
-
         pos = target.position
         bot.pathfinder.setMovements(movements)
         bot.pathfinder.setGoal(pathfinder.goals.GoalNear(pos.x, pos.y, pos.z, RANGE_GOAL))
@@ -39,3 +38,26 @@ def handle(*args):
 @On(bot, "end")
 def handle(*args):
   print("Bot ended!", args)
+
+@On(bot, "login")
+def login(*args):
+    bot.chat("Hi everyone!")
+
+@On(bot, "entityHurt")
+def entityHurt(this, entity):
+    if entity == None:
+       return
+    print(' ', entity)
+    if entity.type == "hostile":
+        bot.chat(f"Haha! The {entity.displayName} got hurt!")
+    elif entity.type == "player":
+        if entity.username in bot.players:
+            ping = bot.players[entity.username].ping
+            bot.chat(f"Aww, poor {entity.username} got hurt. Maybe you shouldn't have a ping of {ping}")
+
+@On(bot, "health")
+def health(*args):
+    bot.chat(f"I have {bot.health} health and {bot.food} food")
+@On(bot, "death")
+def death(*args):
+    bot.chat("I died x.x")
